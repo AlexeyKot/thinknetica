5a3f090d13bd4434bf066e9f71cc5533ad3015e9
Alexey Chernov <shadmas@gmail.com>
2019-05-14 17:37:40 +0300

    Добавил решение задачи

---
+++ Lesson1/square_equation.rb
@@ -0,0 +1,21 @@
+puts "Введите первый коэффициент уравнения (a)"
+a = gets.chomp.to_f
+puts "Введите первый коэффициент уравнения (b)"
+b = gets.chomp.to_f
+puts "Введите первый коэффициент уравнения (c)"
+c = gets.chomp.to_f
+
+discr = b**2-4*a*c
+
+puts "Дискриминант равен #{discr}"
+
+if discr > 0
+    x1 = (-b + Math.sqrt(discr)) / (2*a)
+    x2 = (-b - Math.sqrt(discr)) / (2*a)
+    puts "Два корня. X1 = #{x1}; X2 = #{x2}"
+elsif discr == 0
+    x = -b / (2*a)
+    puts "Один корень, X = #{x}"
+elsif discr < 0
+    puts "Уравнение действительных решений не имеет"
+end
