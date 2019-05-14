9393d8905b5a3701a1dfe437ba46d6d055e1d5ed
Alexey Chernov <shadmas@gmail.com>
2019-05-14 06:38:09 +0300

    Сдела задание про идеальный вес

---
+++ Lesson1/perfect_weight.rb
@@ -0,0 +1,10 @@
+puts "Как вас зовут?"
+name = gets.chomp
+puts "А какой у вас рост?"
+height = gets.chomp.to_i
+perfect_weight = height - 110
+if perfect_weight > 0
+    puts "#{name}, ваш идеальный вес - #{perfect_weight}кг."
+else
+    puts "Вес уже оптимальный"
+end
