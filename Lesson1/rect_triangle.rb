15385ee340901b6b849b0a24a25394b5d2a02a10
Alexey Chernov <shadmas@gmail.com>
2019-05-15 11:11:13 +0300

    Рефакторинг

--- Lesson1/rect_triangle.rb
+++ Lesson1/rect_triangle.rb
@@ -9,15 +9,13 @@ triangle_sides.push(gets.chomp.to_f)
 puts "Введите длину третьей стороны:"
 triangle_sides.push(gets.chomp.to_f)
 
-triangle_sides.sort!
+a, b, h = triangle_sides.sort
 
-hypotenuse = triangle_sides.last
+is_rectangular = hypotenuse ** 2 == a ** 2 + n ** 2
 
-is_rectangular = hypotenuse ** 2 == triangle_sides[0] ** 2 + triangle_sides[1] ** 2
+is_isosceles = a == b || b == h
 
-is_isosceles = triangle_sides[0] == triangle_sides[1] || triangle_sides[1] == triangle_sides[2]
-
-is_equilateral = triangle_sides[0] == triangle_sides[1] && triangle_sides[0] == triangle_sides[2]
+is_equilateral = a == b && a == h
 
 if is_rectangular && is_isosceles
   puts "Треугольник прямоугольный и равнобедренный."
